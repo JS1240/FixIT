61cf9023a2a2f8b0080d9c026e1ed6d6e385d701
Jure Sunic <juresunic@hotmail.com>
2022-03-30 03:54:51 +0100

    <Log in> Created a log in check controller Made a log in view controller check if the person is already signed up, so it can access the app by logging in, if not it shows an error message.

--- FixIT/Controllers/LogInViewController.swift
+++ FixIT/Controllers/LogInViewController.swift
@@ -6,13 +6,25 @@
 //
 
 import UIKit
+import Firebase
 
 class LogInViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    // Lo In button is pressed and it checks for the users in the database with the email and password auth
+    @IBAction func logInPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if error != nil {
+                    self.errorLabel.text = "Email or password is incorrect!"
+                } else {
+                    // Navigate to the main menu
+                    self.performSegue(withIdentifier: "LogInToMainMenu", sender: self)
+                    }
+            }
+        }
     }
-
-
 }
