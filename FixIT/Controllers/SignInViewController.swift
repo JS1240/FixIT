3a5a547441f5964c8a101f92a7bb74ec45bb8e6d
Jure Sunic <juresunic@hotmail.com>
2022-03-30 02:35:21 +0100

    <Backend> Added Firebase to the project Added Firebase as backend to the project and connected the sign in option with it, so now the user is able to register with the email and password to the application.

--- FixIT/Controllers/SignInViewController.swift
+++ FixIT/Controllers/SignInViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import Firebase
 
 class SignInViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var fullNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func signUpPressed(_ sender: UIButton) {
+        
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            // Checks if the user has inputed email and password into the text field to continue
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let e = error {
+                print(e.localizedDescription)
+            } else {
+                // Navigate to the main menu
+                self.performSegue(withIdentifier: "SignInToMainMenu", sender: self)
+                }
+            }
+        }
     }
-
-
 }
 
