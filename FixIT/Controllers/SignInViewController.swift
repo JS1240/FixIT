7fe8ac328f1fbbbe5e5ff88d25980c68fd3e65eb
Jure Sunic <juresunic@hotmail.com>
2022-03-30 03:07:12 +0100

    <Error> Inserted error label This lets user know if the error happens in the sign in process.

--- FixIT/Controllers/SignInViewController.swift
+++ FixIT/Controllers/SignInViewController.swift
@@ -13,14 +13,15 @@ class SignInViewController: UIViewController {
     @IBOutlet weak var fullNameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
     
     @IBAction func signUpPressed(_ sender: UIButton) {
         
         if let email = emailTextField.text, let password = passwordTextField.text {
             // Checks if the user has inputed email and password into the text field to continue
         Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-            if let e = error {
-                print(e.localizedDescription)
+            if error != nil {
+                self.errorLabel.text = "There is a problem with one of the inputs!"
             } else {
                 // Navigate to the main menu
                 self.performSegue(withIdentifier: "SignInToMainMenu", sender: self)
