dbb8a4753fb40ae26b82eae3a7a88f0932104a45
Jure Sunic <juresunic@hotmail.com>
2022-04-07 15:59:02 +0100

    <Cell> Added a custom collection cell and kingfisher library Made a cell for the collection of the service categories for the home menu and added a kingfisher library which allows downloading and caching images from the web.

---
+++ FixIT/Models/ServiceCategory.swift
@@ -0,0 +1,12 @@
+//
+//  ServiceCategory.swift
+//  FixIT
+//
+//  Created by Jure Šunić on 07/04/2022.
+//
+
+import Foundation
+
+struct ServiceCategory {
+    let id, name, image: String
+}
---
+++ FixIT/Views/Category Collection View/CategoryCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryCollectionViewCell.swift
+//  FixIT
+//
+//  Created by Jure Šunić on 07/04/2022.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var categoryImageView: UIImageView!
+    @IBOutlet weak var categoryTitleLabel: UILabel!
+    
+    func setup(category: ServiceCategory) {
+        
+    }
+}
